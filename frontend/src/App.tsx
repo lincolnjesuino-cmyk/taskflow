b2a044ad4d8e0cb0c960a95c79201cb66340fb21
Lincoln Massari <lincolnjesuino@gmail.com>
2026-07-13 08:34:48 -0300

    feat: renderiza lista de tarefas com useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css'
 import Header from './Header'
+import {useState} from 'react'    
 
 function App() {
+  const [tarefas, setTarefas] = useState(["Estudar TS", "Fazer compras"])
   return (
     <>
         <Header titulo="TaskFlow" />
+
+        <ul>
+          {tarefas.map((tarefa) => (
+          <li key={tarefa}>{tarefa}</li>
+      ))}
+          </ul>
+
+
     </>
   )
 }
