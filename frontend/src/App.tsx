022855189fda2ac2ad9de017a95c9d6a8be0b2b4
Lincoln Massari <lincolnjesuino@gmail.com>
2026-07-13 09:11:15 -0300

    feat: transforma tarefa em objeto tipado com id e status

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,33 @@
 import './App.css'
 import Header from './Header'
-import {useState} from 'react'    
+import {useState} from 'react'   
+
+type Tarefa = {
+  id: number
+  texto: string
+  concluida: boolean
+}
 
 function App() {
-  const [tarefas, setTarefas] = useState(["Estudar TS", "Fazer compras"])
   const [texto, setTexto] = useState("")
+  const [tarefas, setTarefas] = useState<Tarefa[]>([
+    {id: 1, texto: "Estudar TS", concluida: false},
+    {id: 2, texto: "Fazer compras", concluida: false}
+
+  ])
   return (
     <>
         <Header titulo="TaskFlow" />
 
         <ul>
           {tarefas.map((tarefa) => (
-          <li key={tarefa}>{tarefa}</li>
+          <li key={tarefa.id}>{tarefa.texto}</li>
       ))}
           </ul>
 
         <input type="text" value={texto} onChange={(e) => setTexto(e.target.value)} />
         <button onClick={() => {
-          setTarefas([...tarefas, texto])
+          setTarefas([...tarefas, {id: tarefas.length + 1, texto, concluida: false}])
           setTexto("")
         }}>Adicionar</button>
 
