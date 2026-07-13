d8d5b45ab8716911e9fa149b673b97763eb3d6e7
Lincoln Massari <lincolnjesuino@gmail.com>
2026-07-13 08:56:24 -0300

    feat: adiciona input para criar novas tarefas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import {useState} from 'react'
 
 function App() {
   const [tarefas, setTarefas] = useState(["Estudar TS", "Fazer compras"])
+  const [texto, setTexto] = useState("")
   return (
     <>
         <Header titulo="TaskFlow" />
@@ -13,6 +14,12 @@ function App() {
           <li key={tarefa}>{tarefa}</li>
       ))}
           </ul>
+
+        <input type="text" value={texto} onChange={(e) => setTexto(e.target.value)} />
+        <button onClick={() => {
+          setTarefas([...tarefas, texto])
+          setTexto("")
+        }}>Adicionar</button>
 
 
     </>
