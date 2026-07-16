9a046e38f1b182ae934477ee6be553f31006e284
Lincoln Massari <lincolnjesuino@gmail.com>
2026-07-16 14:15:54 -0300

    feat: adiciona rotas PATCH e DELETE, completando CRUD do backend

--- backend/src/routes/tarefas.ts
+++ backend/src/routes/tarefas.ts
@@ -10,15 +10,35 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   const { texto, userId } = req.body
-
   const novaTarefa = await prisma.tarefa.create({
     data: {
       texto,
       userId,
     },
   })
-
   res.status(201).json(novaTarefa)
+})
+
+router.patch('/:id', async (req, res) => {
+  const { id } = req.params
+  const { status } = req.body
+
+  const tarefaAtualizada = await prisma.tarefa.update({
+    where: { id: Number(id) },
+    data: { status },
+  })
+
+  res.json(tarefaAtualizada)
+})
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+
+  await prisma.tarefa.delete({
+    where: { id: Number(id) },
+  })
+
+  res.status(204).send()
 })
 
 export default router
